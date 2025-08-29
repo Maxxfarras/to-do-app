ca1cdedd7ea657f139e8dbeff7dc931c31d92c91
maxxfarras <pacmanhead06@gmail.com>
2025-08-29 12:16:00 -0700

    add Task obj to organize better the tasks
    add database vec
    this is only temporary, maybe change to struct
    
    seems to work fine

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,18 @@ fn main() {
         process::exit(1);
     });
 
-    println!("{:#?}", config);
+    let task1 = Task::build(config).unwrap_or_else(|err| {
+        eprintln!("Error creating the task: {err}");
+        process::exit(1);
+    });
+
+    let mut database: Vec<Task> = Vec::new();
+
+    database.push(task1);
+
+    println!("{:#?}", database);
+
+
 }
 
 #[derive(Debug)]
@@ -30,4 +41,26 @@ impl Config {
 
         Ok(Self { command, argument})
     }
+
+}
+
+#[derive(Debug)]
+struct Task {
+    description: String,
+    done: bool,
+}
+
+impl Task {
+
+    pub fn build(config: Config) -> Result<Self, &'static str> {
+        let description = match config.argument {
+            Some(arg) => arg,
+            None => return Err("No argument provided"),
+        };
+
+        let done = false;
+
+        Ok(Self { description, done })
+    }
+
 }
