bbdd87eb1699ca114eacc517026e7f7e1703f102
maxxfarras <pacmanhead06@gmail.com>
2025-08-29 11:28:33 -0700

    Add Config obj Working on the parsing of the commands by using a config obj to save the command and argument.
    seems to  work fine

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,33 @@
+use std::{env, process};
+
 fn main() {
-    println!("Hello, world!");
+
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Error parsing the commands: {err}");
+        process::exit(1);
+    });
+
+    println!("{:#?}", config);
 }
 
+#[derive(Debug)]
+struct Config {
+    command: String,
+    argument: Option<String>,
+}
+
+impl Config {
+
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Self, &'static str> {
+        args.next();
+
+        let command = match args.next() {
+            Some(arg) => arg,
+            None => return Err("No command parsed!!")
+        };
+
+        let argument = args.next();
+
+        Ok(Self { command, argument})
+    }
+}
